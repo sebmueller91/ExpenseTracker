f7894ecd463544f97763bbaa43d76f47b1636ac4
Sebastian Müller <seb.mueller.91@web.de>
2023-10-16 22:00:38 +0200

    Added lazy column

--- app/src/main/java/com/example/expensetracker/model/Currency.kt
+++ app/src/main/java/com/example/expensetracker/model/Currency.kt
@@ -1,5 +1,7 @@
 package com.example.expensetracker.model
 
+val CURRENCIES = listOf(Currency(name = "Euro", abbreviation = "EUR", symbol = '€'))
+
 data class Currency(
     val name: String,
     val abbreviation: String,
