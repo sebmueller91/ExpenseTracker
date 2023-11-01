8907261f8d714268ffb8079bc58b1f329791b2af
Sebastian Müller <seb.mueller.91@web.de>
2023-11-01 23:05:00 +0100

    Extended design

--- app/src/main/java/com/example/expensetracker/ui/screens/EventsOverviewScreen.kt
+++ app/src/main/java/com/example/expensetracker/ui/screens/EventsOverviewScreen.kt
@@ -1,29 +1,40 @@
 package com.example.expensetracker.ui.screens
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.example.expensetracker.R
 import com.example.expensetracker.model.CURRENCIES
 import com.example.expensetracker.model.Event
 import com.example.expensetracker.model.Participant
-import com.example.expensetracker.ui.screens.destinations.EventDetailScreenDestination
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -34,15 +45,14 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 fun EventsOverviewScreen(
     navigator: DestinationsNavigator
 ) {
-    Button(onClick = {
-        navigator.navigate(EventDetailScreenDestination())
-    }) {
-        Text("Go to details")
-    }
-
     val events = listOf(
         Event(
             currency = CURRENCIES.get(0), name = "Summer Breeze",
+            participants = listOf(Participant("Dennis"), Participant("Johnny")),
+            transactions = listOf()
+        ),
+        Event(
+            currency = CURRENCIES.get(0), name = "Rock im Park",
             participants = listOf(Participant("Dennis"), Participant("Johnny")),
             transactions = listOf()
         )
@@ -61,10 +71,12 @@ fun EventsOverviewScreen(
             contentScale = ContentScale.Crop
         )
         LazyColumn(
-            modifier = Modifier.background(MaterialTheme.colors.background)
+            modifier = Modifier
+                .background(MaterialTheme.colors.background)
+                .padding(horizontal = 24.dp)
         ) {
             items(items = events) {
-                EventCard(eventName = it.name)
+                EventCard(event = it)
             }
         }
     }
@@ -72,13 +84,46 @@ fun EventsOverviewScreen(
 
 @Composable
 private fun EventCard(
-    eventName: String,
-    modifier: Modifier = Modifier
+    event: Event,
+    modifier: Modifier = Modifier.fillMaxWidth()
 ) {
     Card(
         elevation = 4.dp,
         modifier = modifier.padding(8.dp)
     ) {
-        Text(eventName)
+        var expanded by remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessMedium
+                )
+            )
+        ) {
+            Row() {
+                Text(event.name)
+                ExpandCollapseButton(expanded = expanded, onClick = { expanded = !expanded })
+            }
+            if (expanded) {
+                for (participant in event.participants) {
+                    Text(participant.name)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ExpandCollapseButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = null
+        )
     }
 }
