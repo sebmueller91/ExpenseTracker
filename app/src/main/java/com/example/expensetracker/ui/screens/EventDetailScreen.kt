aee7f4081367708a16b9c9c5b0cffddf24ce8f52
Sebastian Müller <seb.mueller.91@web.de>
2023-11-03 12:37:13 +0100

    Added Draft for AddEventScreen

--- app/src/main/java/com/example/expensetracker/ui/screens/EventDetailScreen.kt
+++ app/src/main/java/com/example/expensetracker/ui/screens/EventDetailScreen.kt
@@ -4,9 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import com.example.expensetracker.ui.screens.destinations.EventDetailScreenDestination
 import com.example.expensetracker.ui.screens.destinations.EventsOverviewScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
