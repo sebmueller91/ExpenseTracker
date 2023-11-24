e661359182e0c756fe68192106fa63478dd42267
Sebastian Mueller <sebastian.mueller@sivantos.com>
2023-11-24 17:36:17 +0100

    improvded add group screen

--- app/src/main/java/com/example/expensetracker/ui/screens/add_group/AddGroupUiState.kt
+++ app/src/main/java/com/example/expensetracker/ui/screens/add_group/AddGroupUiState.kt
@@ -3,7 +3,7 @@ package com.example.expensetracker.ui.screens.add_group
 import com.example.expensetracker.model.Currency
 
 data class AddGroupUiState(
-    val showScreen1: Boolean = true, // TODO: Better name?
+    val subScreen: AddGroupSubScreens = AddGroupSubScreens.GROUPNAME_CURRENCY,
     val groupName: String = "",
     val participantsNames: List<String> = listOf(""),
     val currency: Currency = Currency.EURO
