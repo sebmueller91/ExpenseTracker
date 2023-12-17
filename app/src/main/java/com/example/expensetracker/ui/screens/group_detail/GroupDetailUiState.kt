db0a0d64937b150a93fa144a510a44fd27a41201
Sebastian Müller <seb.mueller.91@web.de>
2023-12-17 16:00:17 +0100

    Added FormattedTransaction

--- app/src/main/java/com/example/expensetracker/ui/screens/group_detail/GroupDetailUiState.kt
+++ app/src/main/java/com/example/expensetracker/ui/screens/group_detail/GroupDetailUiState.kt
@@ -3,7 +3,9 @@ package com.example.expensetracker.ui.screens.group_detail
 import com.example.expensetracker.model.Group
 
 sealed class GroupDetailUiState {
-    data class Success(val group: Group): GroupDetailUiState()
-    object Loading: GroupDetailUiState()
-    object Error: GroupDetailUiState()
+    data class Success(val group: Group) :
+        GroupDetailUiState()
+
+    data object Loading : GroupDetailUiState()
+    data object Error : GroupDetailUiState()
 }
