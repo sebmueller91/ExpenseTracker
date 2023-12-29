a9f91d449271b95bfc25ec490c72d3ee82dd2346
Sebastian Müller <seb.mueller.91@web.de>
2023-12-29 12:24:26 +0100

    Use Timber for Logging

--- app/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.kt
+++ app/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.kt
@@ -7,10 +7,15 @@ import com.example.expensetracker.use_cases.di.useCasesModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class ExpenseTrackerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         startKoin {
             androidLogger()
