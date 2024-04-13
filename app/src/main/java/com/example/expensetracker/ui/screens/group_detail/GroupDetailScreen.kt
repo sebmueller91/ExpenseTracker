48779a845fbce2a82cb690fa9b96d7a9920de3e8
Sebastian Müller <seb.mueller.91@web.de>
2024-04-13 15:28:40 +0200

    Remove animation between detail tabs

--- app/src/main/java/com/example/expensetracker/ui/screens/group_detail/GroupDetailScreen.kt
+++ app/src/main/java/com/example/expensetracker/ui/screens/group_detail/GroupDetailScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.BarChart
@@ -25,15 +23,14 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.expensetracker.R
 import com.example.core.model.Transaction
+import com.example.expensetracker.R
 import com.example.expensetracker.ui.components.NavigationIcon
 import com.example.expensetracker.ui.screens.destinations.GroupOverviewScreenDestination
 import com.example.expensetracker.ui.screens.group_detail.tabs.ExpensesTab
@@ -41,7 +38,6 @@ import com.example.expensetracker.ui.screens.group_detail.tabs.SettleUpTab
 import com.example.expensetracker.ui.screens.group_detail.tabs.StatisticsTab
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
 import java.util.UUID
@@ -98,8 +94,6 @@ private fun GroupDetailScreenContent(
     onLeave: () -> Unit
 ) {
     var selectedTab by remember { mutableStateOf(GroupDetailScreenTabs.EXPENSES) }
-    val pagerState = rememberPagerState { GroupDetailScreenTabs.entries.size }
-    val coroutineScope = rememberCoroutineScope()
 
     Scaffold(modifier = modifier
         .fillMaxSize(),
@@ -112,13 +106,10 @@ private fun GroupDetailScreenContent(
         },
         bottomBar = {
             NavigationBar {
-                GroupDetailScreenTabs.entries.forEachIndexed { index, tab ->
+                GroupDetailScreenTabs.entries.forEach { tab ->
                     NavigationBarItem(
-                        selected = pagerState.currentPage == index,
+                        selected = selectedTab == tab,
                         onClick = {
-                            coroutineScope.launch {
-                                pagerState.animateScrollToPage(index)
-                            }
                             selectedTab = tab
                         },
                         icon = {
@@ -132,17 +123,15 @@ private fun GroupDetailScreenContent(
             }
         })
     { innerPadding ->
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) { page ->
-            when (GroupDetailScreenTabs.entries[page]) {
-                GroupDetailScreenTabs.EXPENSES -> ExpensesTab(uiState)
-                GroupDetailScreenTabs.STATISTICS -> StatisticsTab(uiState)
-                GroupDetailScreenTabs.SETTLE_UP -> SettleUpTab(uiState = uiState, applySettleUpTransaction = applySettleUpTransaction)
-            }
+        val paddingModifier = Modifier.padding(innerPadding)
+        when (selectedTab) {
+            GroupDetailScreenTabs.EXPENSES -> ExpensesTab(uiState, modifier = paddingModifier)
+            GroupDetailScreenTabs.STATISTICS -> StatisticsTab(uiState, modifier = paddingModifier)
+            GroupDetailScreenTabs.SETTLE_UP -> SettleUpTab(
+                uiState = uiState,
+                applySettleUpTransaction = applySettleUpTransaction,
+                modifier = paddingModifier
+            )
         }
     }
 }
